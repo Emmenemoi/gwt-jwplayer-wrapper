8f5044330f00da1741308590d34875d7b1842f7e
Sebastien <sebastien@emmene-moi.fr>
2014-12-10 16:43:10 +0100

    working playlist sources

---
+++ war/fr.emmenemoi.gwt.widgets.jwplayer.example.Gwtjwplayerwrapper/compile-12/war/gwtjwplayerwrapper/gwtjwplayerwrapper.nocache.js
@@ -0,0 +1,309 @@
+function gwtjwplayerwrapper(){
+  var $wnd_0 = window;
+  var $doc_0 = document;
+  sendStats('bootstrap', 'begin');
+  function isHostedMode(){
+    var query = $wnd_0.location.search;
+    return query.indexOf('gwt.codesvr.gwtjwplayerwrapper=') != -1 || query.indexOf('gwt.codesvr=') != -1;
+  }
+
+  function sendStats(evtGroupString, typeString){
+    if ($wnd_0.__gwtStatsEvent) {
+      $wnd_0.__gwtStatsEvent({moduleName:'gwtjwplayerwrapper', sessionId:$wnd_0.__gwtStatsSessionId, subSystem:'startup', evtGroup:evtGroupString, millis:(new Date).getTime(), type:typeString});
+    }
+  }
+
+  gwtjwplayerwrapper.__sendStats = sendStats;
+  gwtjwplayerwrapper.__moduleName = 'gwtjwplayerwrapper';
+  gwtjwplayerwrapper.__errFn = null;
+  gwtjwplayerwrapper.__moduleBase = 'DUMMY';
+  gwtjwplayerwrapper.__softPermutationId = 0;
+  gwtjwplayerwrapper.__computePropValue = null;
+  gwtjwplayerwrapper.__getPropMap = null;
+  gwtjwplayerwrapper.__gwtInstallCode = function(){
+  }
+  ;
+  gwtjwplayerwrapper.__gwtStartLoadingFragment = function(){
+    return null;
+  }
+  ;
+  var __gwt_isKnownPropertyValue = function(){
+    return false;
+  }
+  ;
+  var __gwt_getMetaProperty = function(){
+    return null;
+  }
+  ;
+  __propertyErrorFunction = null;
+  var activeModules = $wnd_0.__gwt_activeModules = $wnd_0.__gwt_activeModules || {};
+  activeModules['gwtjwplayerwrapper'] = {moduleName:'gwtjwplayerwrapper'};
+  var frameDoc;
+  function getInstallLocationDoc(){
+    setupInstallLocation();
+    return frameDoc;
+  }
+
+  function getInstallLocation(){
+    setupInstallLocation();
+    return frameDoc.getElementsByTagName('body')[0];
+  }
+
+  function setupInstallLocation(){
+    if (frameDoc) {
+      return;
+    }
+    var scriptFrame = $doc_0.createElement('iframe');
+    scriptFrame.src = 'javascript:""';
+    scriptFrame.id = 'gwtjwplayerwrapper';
+    scriptFrame.style.cssText = 'position:absolute; width:0; height:0; border:none; left: -1000px;' + ' top: -1000px;';
+    scriptFrame.tabIndex = -1;
+    $doc_0.body.appendChild(scriptFrame);
+    frameDoc = scriptFrame.contentDocument;
+    if (!frameDoc) {
+      frameDoc = scriptFrame.contentWindow.document;
+    }
+    frameDoc.open();
+    var doctype = document.compatMode == 'CSS1Compat'?'<!doctype html>':'';
+    frameDoc.write(doctype + '<html><head><\/head><body><\/body><\/html>');
+    frameDoc.close();
+  }
+
+  function installScript(filename){
+    function setupWaitForBodyLoad(callback){
+      function isBodyLoaded(){
+        if (typeof $doc_0.readyState == 'undefined') {
+          return typeof $doc_0.body != 'undefined' && $doc_0.body != null;
+        }
+        return /loaded|complete/.test($doc_0.readyState);
+      }
+
+      var bodyDone = isBodyLoaded();
+      if (bodyDone) {
+        callback();
+        return;
+      }
+      function onBodyDone(){
+        if (!bodyDone) {
+          bodyDone = true;
+          callback();
+          if ($doc_0.removeEventListener) {
+            $doc_0.removeEventListener('DOMContentLoaded', onBodyDone, false);
+          }
+          if (onBodyDoneTimerId) {
+            clearInterval(onBodyDoneTimerId);
+          }
+        }
+      }
+
+      if ($doc_0.addEventListener) {
+        $doc_0.addEventListener('DOMContentLoaded', onBodyDone, false);
+      }
+      var onBodyDoneTimerId = setInterval(function(){
+        if (isBodyLoaded()) {
+          onBodyDone();
+        }
+      }
+      , 50);
+    }
+
+    function installCode(code_0){
+      var docbody = getInstallLocation();
+      var script = getInstallLocationDoc().createElement('script');
+      script.language = 'javascript';
+      script.src = code_0;
+      sendStats('moduleStartup', 'moduleRequested');
+      docbody.appendChild(script);
+    }
+
+    setupWaitForBodyLoad(function(){
+      installCode(filename);
+    }
+    );
+  }
+
+  gwtjwplayerwrapper.__startLoadingFragment = function(fragmentFile){
+    return computeUrlForResource(fragmentFile);
+  }
+  ;
+  gwtjwplayerwrapper.__installRunAsyncCode = function(code_0){
+    var docbody = getInstallLocation();
+    var script = getInstallLocationDoc().createElement('script');
+    script.language = 'javascript';
+    script.text = code_0;
+    docbody.appendChild(script);
+  }
+  ;
+  function processMetas(){
+    var metaProps = {};
+    var propertyErrorFunc;
+    var onLoadErrorFunc;
+    var metas = $doc_0.getElementsByTagName('meta');
+    for (var i = 0, n = metas.length; i < n; ++i) {
+      var meta = metas[i], name_1 = meta.getAttribute('name'), content_0;
+      if (name_1) {
+        name_1 = name_1.replace('gwtjwplayerwrapper::', '');
+        if (name_1.indexOf('::') >= 0) {
+          continue;
+        }
+        if (name_1 == 'gwt:property') {
+          content_0 = meta.getAttribute('content');
+          if (content_0) {
+            var value_1, eq = content_0.indexOf('=');
+            if (eq >= 0) {
+              name_1 = content_0.substring(0, eq);
+              value_1 = content_0.substring(eq + 1);
+            }
+             else {
+              name_1 = content_0;
+              value_1 = '';
+            }
+            metaProps[name_1] = value_1;
+          }
+        }
+         else if (name_1 == 'gwt:onPropertyErrorFn') {
+          content_0 = meta.getAttribute('content');
+          if (content_0) {
+            try {
+              propertyErrorFunc = eval(content_0);
+            }
+             catch (e) {
+              alert('Bad handler "' + content_0 + '" for "gwt:onPropertyErrorFn"');
+            }
+          }
+        }
+         else if (name_1 == 'gwt:onLoadErrorFn') {
+          content_0 = meta.getAttribute('content');
+          if (content_0) {
+            try {
+              onLoadErrorFunc = eval(content_0);
+            }
+             catch (e) {
+              alert('Bad handler "' + content_0 + '" for "gwt:onLoadErrorFn"');
+            }
+          }
+        }
+      }
+    }
+    __gwt_getMetaProperty = function(name_0){
+      var value_0 = metaProps[name_0];
+      return value_0 == null?null:value_0;
+    }
+    ;
+    __propertyErrorFunction = propertyErrorFunc;
+    gwtjwplayerwrapper.__errFn = onLoadErrorFunc;
+  }
+
+  function computeScriptBase(){
+    $wnd_0.__gwt_activeModules['gwtjwplayerwrapper'].superdevmode = true;
+    var expectedSuffix = '/gwtjwplayerwrapper.nocache.js';
+    var scriptTags = $doc_0.getElementsByTagName('script');
+    for (var i = 0;; i++) {
+      var tag = scriptTags[i];
+      if (!tag) {
+        break;
+      }
+      var candidate = tag.src;
+      var lastMatch = candidate.lastIndexOf(expectedSuffix);
+      if (lastMatch == candidate.length - expectedSuffix.length) {
+        return candidate.substring(0, lastMatch + 1);
+      }
+    }
+    $wnd_0.alert('Unable to load Super Dev Mode version of ' + gwtjwplayerwrapper + '.');
+  }
+
+  function computeUrlForResource(resource){
+    if (resource.match(/^\//)) {
+      return resource;
+    }
+    if (resource.match(/^[a-zA-Z]+:\/\//)) {
+      return resource;
+    }
+    return gwtjwplayerwrapper.__moduleBase + resource;
+  }
+
+  function getCompiledCodeFilename(){
+    var answers = [];
+    var softPermutationId;
+    var values = [];
+    var providers = [];
+    function computePropValue(propName){
+      var value_0 = providers[propName](), allowedValuesMap = values[propName];
+      if (value_0 in allowedValuesMap) {
+        return value_0;
+      }
+      var allowedValuesList = [];
+      for (var k in allowedValuesMap) {
+        allowedValuesList[allowedValuesMap[k]] = k;
+      }
+      if (__propertyErrorFunc) {
+        __propertyErrorFunc(propName, allowedValuesList, value_0);
+      }
+      throw null;
+    }
+
+    __gwt_isKnownPropertyValue = function(propName, propValue){
+      return propValue in values[propName];
+    }
+    ;
+    gwtjwplayerwrapper.__getPropMap = function(){
+      var result = {};
+      for (var key in values) {
+        if (values.hasOwnProperty(key)) {
+          result[key] = computePropValue(key);
+        }
+      }
+      return result;
+    }
+    ;
+    gwtjwplayerwrapper.__computePropValue = computePropValue;
+    $wnd_0.__gwt_activeModules['gwtjwplayerwrapper'].bindings = gwtjwplayerwrapper.__getPropMap;
+    sendStats('bootstrap', 'selectingPermutation');
+    if (isHostedMode()) {
+      return computeUrlForResource('gwtjwplayerwrapper.devmode.js');
+    }
+    var strongName;
+    try {
+      strongName = 'DD132DA7D6E4C10910615389550B89BA';
+      var idx = strongName.indexOf(':');
+      if (idx != -1) {
+        softPermutationId = parseInt(strongName.substring(idx + 1), 10);
+        strongName = strongName.substring(0, idx);
+      }
+    }
+     catch (e) {
+    }
+    gwtjwplayerwrapper.__softPermutationId = softPermutationId;
+    return computeUrlForResource(strongName + '.cache.js');
+  }
+
+  function loadExternalStylesheets(){
+    if (!$wnd_0.__gwt_stylesLoaded) {
+      $wnd_0.__gwt_stylesLoaded = {};
+    }
+    function installOneStylesheet(stylesheetUrl){
+      if (!__gwt_stylesLoaded[stylesheetUrl]) {
+        var l = $doc_0.createElement('link');
+        l.setAttribute('rel', 'stylesheet');
+        l.setAttribute('href', computeUrlForResource(stylesheetUrl));
+        $doc_0.getElementsByTagName('head')[0].appendChild(l);
+        __gwt_stylesLoaded[stylesheetUrl] = true;
+      }
+    }
+
+    sendStats('loadExternalRefs', 'begin');
+    installOneStylesheet('gwt/standard/standard.css');
+    sendStats('loadExternalRefs', 'end');
+  }
+
+  processMetas();
+  gwtjwplayerwrapper.__moduleBase = computeScriptBase();
+  activeModules['gwtjwplayerwrapper'].moduleBase = gwtjwplayerwrapper.__moduleBase;
+  var filename_0 = getCompiledCodeFilename();
+  loadExternalStylesheets();
+  sendStats('bootstrap', 'end');
+  installScript(filename_0);
+  return true;
+}
+
+gwtjwplayerwrapper.succeeded = gwtjwplayerwrapper();
---
+++ war/fr.emmenemoi.gwt.widgets.jwplayer.example.Gwtjwplayerwrapper/compile-22/gen/fr/emmenemoi/gwt/widgets/jwplayer/example/client/place/fr_emmenemoi_gwt_widgets_jwplayer_example_client_gin_ClientGinjector_ClientGinjectorGinjector_fragment.java
@@ -0,0 +1,37 @@
+package fr.emmenemoi.gwt.widgets.jwplayer.example.client.place;
+
+import com.google.gwt.core.client.GWT;
+import fr.emmenemoi.gwt.widgets.jwplayer.example.client.gin.fr_emmenemoi_gwt_widgets_jwplayer_example_client_gin_ClientGinjector_ClientGinjectorGinjector;
+
+public class fr_emmenemoi_gwt_widgets_jwplayer_example_client_gin_ClientGinjector_ClientGinjectorGinjector_fragment {
+  public void memberInject_Key$type$fr$emmenemoi$gwt$widgets$jwplayer$example$client$place$ClientPlaceManager$_annotation$$none$$(fr.emmenemoi.gwt.widgets.jwplayer.example.client.place.ClientPlaceManager injectee) {
+    
+  }
+  
+  
+  /**
+   * Binding for fr.emmenemoi.gwt.widgets.jwplayer.example.client.place.ClientPlaceManager declared at:
+   *   Implicit binding for fr.emmenemoi.gwt.widgets.jwplayer.example.client.place.ClientPlaceManager
+   */
+  public fr.emmenemoi.gwt.widgets.jwplayer.example.client.place.ClientPlaceManager get_Key$type$fr$emmenemoi$gwt$widgets$jwplayer$example$client$place$ClientPlaceManager$_annotation$$none$$() {
+    fr.emmenemoi.gwt.widgets.jwplayer.example.client.place.ClientPlaceManager result = fr$emmenemoi$gwt$widgets$jwplayer$example$client$place$ClientPlaceManager_fr$emmenemoi$gwt$widgets$jwplayer$example$client$place$ClientPlaceManager_methodInjection(injector.getFragment_com_google_web_bindery_event_shared().get_Key$type$com$google$web$bindery$event$shared$EventBus$_annotation$$none$$(), injector.getFragment_com_gwtplatform_mvp_client_proxy().get_Key$type$com$gwtplatform$mvp$client$proxy$TokenFormatter$_annotation$$none$$(), injector.getFragment_fr_emmenemoi_gwt_widgets_jwplayer_example_client_gin().get_Key$type$java$lang$String$_annotation$$fr$emmenemoi$gwt$widgets$jwplayer$example$client$place$DefaultPlace$());
+    memberInject_Key$type$fr$emmenemoi$gwt$widgets$jwplayer$example$client$place$ClientPlaceManager$_annotation$$none$$(result);
+    
+    return result;
+    
+  }
+  
+  public fr.emmenemoi.gwt.widgets.jwplayer.example.client.place.ClientPlaceManager fr$emmenemoi$gwt$widgets$jwplayer$example$client$place$ClientPlaceManager_fr$emmenemoi$gwt$widgets$jwplayer$example$client$place$ClientPlaceManager_methodInjection(com.google.web.bindery.event.shared.EventBus _0, com.gwtplatform.mvp.client.proxy.TokenFormatter _1, java.lang.String _2) {
+    return new fr.emmenemoi.gwt.widgets.jwplayer.example.client.place.ClientPlaceManager(_0, _1, _2);
+  }
+  
+  
+  /**
+   * Field for the enclosing injector.
+   */
+  private final fr_emmenemoi_gwt_widgets_jwplayer_example_client_gin_ClientGinjector_ClientGinjectorGinjector injector;
+  public fr_emmenemoi_gwt_widgets_jwplayer_example_client_gin_ClientGinjector_ClientGinjectorGinjector_fragment(fr_emmenemoi_gwt_widgets_jwplayer_example_client_gin_ClientGinjector_ClientGinjectorGinjector injector) {
+    this.injector = injector;
+  }
+  
+}
